c76bc522f1cec894f1c541617ce8bb4beeb8b6ac
Wilson Benitez <wolfwilson1986@gmail.com>
2024-10-21 12:56:02 -0300

    Arreglos varios de la búsqueda

--- js/search.js
+++ js/search.js
@@ -5,12 +5,14 @@ document.getElementById('search-button').addEventListener('click', function(even
     event.preventDefault(); // Evitar la recarga de la página
 
     // Obtener el valor del cuadro de búsqueda
-    const query = document.getElementById('search-input').value.toLowerCase();
+    const query = document.getElementById('search-input').value.trim().toLowerCase();
 
     if (query) {
         // Guardar el término de búsqueda en localStorage
         localStorage.setItem('searchQuery', query);
         // Redirigir a la página de resultados
         window.location.href = 'resultados.html';
+    } else {
+        alert("Por favor, ingrese un término de búsqueda.");
     }
 });
